d6c57fff18839649ec68012289aa76187e97edff
SabinaAlieva <sabina-alieva.taganrog@mail.com>
2016-12-07 20:08:59 +0300

    Программа готова. Удаляет процессы, на кторых было последнее нажатие мыши в таблице.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ProcessManager
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.Table1 = new System.Windows.Forms.DataGridView();
+            this.Table2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(12, 357);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(338, 60);
+            this.buttonUpdate.TabIndex = 0;
+            this.buttonUpdate.Text = "Update List Process";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Table1
+            // 
+            this.Table1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table1.Location = new System.Drawing.Point(12, 65);
+            this.Table1.Name = "Table1";
+            this.Table1.Size = new System.Drawing.Size(338, 286);
+            this.Table1.TabIndex = 3;
+            // 
+            // Table2
+            // 
+            this.Table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table2.Location = new System.Drawing.Point(356, 65);
+            this.Table2.Name = "Table2";
+            this.Table2.Size = new System.Drawing.Size(348, 286);
+            this.Table2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(135, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "All processes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(478, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Killed Processes";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(346, 59);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Kill Process ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // MainWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(715, 429);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Table2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.Table1);
+            this.Name = "MainWindow";
+            this.Text = "Process Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.DataGridView Table1;
+        private System.Windows.Forms.DataGridView Table2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessManager
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Table1.ColumnCount = 3;
+            Table2.ColumnCount = 3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //нажатие на кнопку обновить список процессов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Table1.Rows.Clear();
+            System.Diagnostics.Process[] processes;
+            processes = System.Diagnostics.Process.GetProcesses();
+
+            int pCount = processes.Length;
+            for (int i = 0; i < pCount; i++)
+            {
+                Table1.Rows.Add(processes[i].ProcessName, processes[i].PrivateMemorySize, processes[i].Id );
+            } 
+        }
+
+        //нажатие на кнопку удалить процесс
+        //будет удален тот, на который последний раз нажали левой клавишей мыши в первой таблице
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            int a = Table1.CurrentRow.Index;
+            Table2.Rows.Add(Table1[0, a].Value.ToString(), Table1[1, a].Value.ToString(), Table1[2, a].Value.ToString());
+            System.Diagnostics.Process localById = System.Diagnostics.Process.GetProcessById(int.Parse(Table1[2, a].Value.ToString()));
+            localById.Kill();
+            Table1.Rows.Remove(Table1.Rows[a]);
+        }
+    }
+}
